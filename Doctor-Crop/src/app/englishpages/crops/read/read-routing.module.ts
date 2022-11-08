a5efe501df87e859030b9659e7717059ad02b0bb
KrishanFdo <krishanfernando129@gmail.com>
2022-11-08 18:17:29 +0530

    read page created inside crops page

--- Doctor-Crop/src/app/englishpages/crops/read/read-routing.module.ts
+++ Doctor-Crop/src/app/englishpages/crops/read/read-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CropsPage } from './crops.page';
+import { ReadPage } from './read.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: CropsPage
-  },
-  {
-    path: 'read',
-    loadChildren: () => import('./read/read.module').then( m => m.ReadPageModule)
+    component: ReadPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class CropsPageRoutingModule {}
+export class ReadPageRoutingModule {}
