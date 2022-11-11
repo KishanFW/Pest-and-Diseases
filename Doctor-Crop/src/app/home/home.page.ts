c23b117bab00aad943dad8fda07b1f46e23e5aa8
iresha1234 <116697535+iresha1234@users.noreply.github.com>
2022-11-11 15:34:09 +0530

    alert button created

--- Doctor-Crop/src/app/home/home.page.ts
+++ Doctor-Crop/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  async showAlert() {
+    const alert = await this.alertController.create({
+    
+      
+      message: 'Detect means identifying symptoms after planting.',
 
+      buttons: ['OK'],
+
+
+    });
+
+    await alert.present();
+  }
+
+
+
+async presentAlert() {
+    const alert = await this.alertController.create({
+    
+      
+      message: 'Prevent means following precautions to avoid diseases.',
+
+      buttons: ['OK'],
+
+
+    });
+
+    await alert.present();
+  }
+  
+  
 }
