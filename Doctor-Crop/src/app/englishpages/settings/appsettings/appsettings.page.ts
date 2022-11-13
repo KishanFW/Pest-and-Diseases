87b03a76e15a046a47b3ba07962d5571f95c1e4d
KrishanFdo <krishanfernando129@gmail.com>
2022-11-13 21:44:40 +0530

    appsettings and sappsettings pages completed

--- Doctor-Crop/src/app/englishpages/settings/appsettings/appsettings.page.ts
+++ Doctor-Crop/src/app/englishpages/settings/appsettings/appsettings.page.ts
@@ -15,9 +15,16 @@ export class AppsettingsPage implements OnInit {
   @ViewChild('popover') popover;
 
   isOpen = false;
+  isOpen2 = false;
+
 
   private presentPopover(e: Event) {
     this.popover.event = e;
     this.isOpen = true;
+  }
+
+  private presentPopover2(e: Event) {
+    this.popover.event = e;
+    this.isOpen2 = true;
   }
 }
