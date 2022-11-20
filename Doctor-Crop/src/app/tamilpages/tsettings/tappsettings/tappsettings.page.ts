f6071bda9100064f368b60472c485e2e6fb8a138
KrishanFdo <krishanfernando129@gmail.com>
2022-11-20 11:07:53 +0530

    header footer component routing added in tamil pages

--- Doctor-Crop/src/app/tamilpages/tsettings/tappsettings/tappsettings.page.ts
+++ Doctor-Crop/src/app/tamilpages/tsettings/tappsettings/tappsettings.page.ts
@@ -15,9 +15,15 @@ export class TappsettingsPage implements OnInit {
   @ViewChild('popover') popover;
 
   isOpen = false;
+  isOpen2 = false;
 
   private presentPopover(e: Event) {
     this.popover.event = e;
     this.isOpen = true;
+  }
+
+  private presentPopover2(e: Event) {
+    this.popover.event = e;
+    this.isOpen2 = true;
   }
 }
