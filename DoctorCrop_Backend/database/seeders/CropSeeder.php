5f14304ae3ea802a763cae0c7a546f332e63fd59
KrishanFdo <krishanfernando129@gmail.com>
2022-12-07 11:22:50 +0530

    crop seeder created and included in database seeder

--- DoctorCrop_Backend/database/seeders/CropSeeder.php
+++ DoctorCrop_Backend/database/seeders/CropSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Crop;
 use Illuminate\Database\Seeder;
 
 class CropSeeder extends Seeder
@@ -13,6 +14,25 @@ class CropSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Crop::create([
+            'Crop_Name'=>'Brinjal',
+            'Img_Path'=>'/assets/crops/bri.webp'
+        ]);
+        Crop::create([
+            'Crop_Name'=>'Banana',
+            'Img_Path'=>'/assets/crops/banana.png'
+        ]);
+        Crop::create([
+            'Crop_Name'=>'Bitter Gound',
+            'Img_Path'=>'/assets/crops/bitterground.png'
+        ]);
+        Crop::create([
+            'Crop_Name'=>'Carrot',
+            'Img_Path'=>'/assets/crops/carrot.png'
+        ]);
+        Crop::create([
+            'Crop_Name'=>'Cassava',
+            'Img_Path'=>'/assets/crops/cassava.png'
+        ]);
     }
 }
