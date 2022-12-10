34e18c82330b6fa9bb4d217762d558783949e1a6
pubu <startreknaturalia@gmail.com>
2022-12-10 12:31:02 +0530

    Update CropSeeder.php

--- DoctorCrop_Backend/database/seeders/CropSeeder.php
+++ DoctorCrop_Backend/database/seeders/CropSeeder.php
@@ -15,16 +15,16 @@ class CropSeeder extends Seeder
     public function run()
     {
         Crop::create([
-            'Crop_Name'=>'Brinjal',
-            'Img_Path'=>'/assets/crops/bri.webp'
-        ]);
-        Crop::create([
             'Crop_Name'=>'Banana',
             'Img_Path'=>'/assets/crops/banana.png'
         ]);
         Crop::create([
             'Crop_Name'=>'Bitter Gound',
             'Img_Path'=>'/assets/crops/bitterground.png'
+        ]);
+        Crop::create([
+            'Crop_Name'=>'Brinjal',
+            'Img_Path'=>'/assets/crops/bri.webp'
         ]);
         Crop::create([
             'Crop_Name'=>'Carrot',
