83d236cc7e240789c2dded5d3d30e0a68fa0a3e1
KrishanFdo <krishanfernando129@gmail.com>
2022-12-16 15:01:47 +0530

    all pest details displayed succesfully for brinjal in read option

--- Doctor-Crop/src/app/englishpages/detect/crop-cropvar/crops/pests-diseases/pests/pests.component.ts
+++ Doctor-Crop/src/app/englishpages/detect/crop-cropvar/crops/pests-diseases/pests/pests.component.ts
@@ -1,7 +1,10 @@
+import { PestImg, PestSymptom } from './../pests-diseases.model';
 import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Crop } from '../../crop.model';
 import { ModalController, LoadingController } from '@ionic/angular';
 import { PestsofcropService } from 'src/app/services/pestsofcrop.service';
+import { ImgsofpestService } from 'src/app/services/imgsofpest.service';
+import { SymptomsofpestService } from 'src/app/services/symptomsofpest.service';
 import { Observable } from 'rxjs';
 import { tap } from "rxjs/operators";
 import { Pest } from '../pests-diseases.model';
@@ -14,12 +17,15 @@ import { PestsDiseasesComponent } from '../pests-diseases.component';
 })
 export class PestsComponent implements OnInit {
   @Input() crop: Crop;
-  cropname: string;
 
   pests$: Observable<Pest[]>;
+  imgs$: Observable<PestImg[]>;
+  symptoms$: Observable<PestSymptom[]>
 
   constructor(
     private pestsofcropService: PestsofcropService,
+    private imgsofpestService: ImgsofpestService,
+    private symptomsofpestService: SymptomsofpestService,
     private loadingCtrl: LoadingController,
     private modalCtrl: ModalController,
     private pestdisease: PestsDiseasesComponent
@@ -31,10 +37,22 @@ export class PestsComponent implements OnInit {
 
     this.pests$ = this.pestsofcropService.getPests(this.crop.crop_name).pipe(
       tap((pests) => {
-        loading.dismiss();
         return pests;
         })
     );
+
+    this.symptoms$ = this.symptomsofpestService.getpestsymptoms().pipe(
+        tap((symptoms)=>{
+          return symptoms;
+        })
+    );
+
+    this.imgs$ = this.imgsofpestService.getpestimgs().pipe(
+      tap((imgs)=>{
+        loading.dismiss();
+        return imgs;
+      })
+    )
 
   }
 
--- DoctorCrop_Backend/routes/api.php
+++ DoctorCrop_Backend/routes/api.php
@@ -18,4 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('crops',[ApiController::class,'crops']);
 Route::get('pestsofcrop/{crop}',[ApiController::class,'pestsofcrop']);
-Route::get('pestimgpaths/{pest}',[ApiController::class,'pestsimgpaths']);
+Route::get('pestimgs',[ApiController::class,'pestimgs']);
+Route::get('pestsymptoms',[ApiController::class,'pestsymptoms']);
