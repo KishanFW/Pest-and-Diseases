2c4874a25dd0a93d816429ed2ed388360c98b83b
KrishanFdo <krishanfernando129@gmail.com>
2023-02-21 11:41:20 +0530

    varietydisease and varietypest seeders updated

--- DoctorCrop_Backend/database/seeders/VarietiesDiseaseSeeder.php
+++ DoctorCrop_Backend/database/seeders/VarietiesDiseaseSeeder.php
@@ -14,277 +14,277 @@ class VarietiesDiseaseSeeder extends Seeder
      */
     public function run()
     {
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Damping-Off'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Bacterial wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Amanda F1',
             'disease_name'=>'Little leaf disease'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Damping-Off'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Anjalee-F1',
             'disease_name'=>'Little leaf disease'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Damping-Off'
 
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Bacterial wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'HORDI Lenairi 1',
             'disease_name'=>'Little leaf disease'
 
-        ])
+        ]);
 
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Damping-Off'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Bacterial wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Thinnaweli purple',
             'disease_name'=>'Little leaf disease'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Damping-Off'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'Padagoda(BW-11)',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Damping-Off'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Collar rot'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Fusarium wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Powdery mildew'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Anthracnose'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Phomopsis blight'
 
-        ])
+        ]);
 
-        varieties_pest::create([
+        Varieties_disease::create([
             'variety_name'=>'SM-164',
             'disease_name'=>'Bacterial wilt'
 
-        ])
+        ]);
 
-        varieties_pest::create([
-            'variety_name'=>'SM-164',
-            'disease_name'=>'Little leaf disease'
+        Varieties_disease::create([
+            'variety_name' => 'SM-164',
+            'disease_name' => 'Little leaf disease'
 
-        ])
+        ]);
 
     }
 }
--- DoctorCrop_Backend/database/seeders/VarietiesPestSeeder.php
+++ DoctorCrop_Backend/database/seeders/VarietiesPestSeeder.php
@@ -18,140 +18,140 @@ class VarietiesPestSeeder extends Seeder
             'variety_name'=>'Amanda F1',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Amanda F1',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Amanda F1',
             'pest_name'=>'Mite damage'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Anjalee-F1',
             'pest_name'=>'Borer and fruit borer'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Anjalee-F1',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Anjalee-F1',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Anjalee-F1',
             'pest_name'=>'Mite damage'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'HORDI Lenairi 1',
             'pest_name'=>'Borer and fruit borer'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'HORDI Lenairi 1',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'HORDI Lenairi 1',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
 
         varieties_pest::create([
             'variety_name'=>'HORDI Lenairi 1',
             'pest_name'=>'Mite damage'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Thinnaweli purple',
             'pest_name'=>'Borer and fruit borer'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Thinnaweli purple',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Thinnaweli purple',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Thinnaweli purple',
             'pest_name'=>'Mite damage'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Padagoda(BW-11)',
             'pest_name'=>'Borer and fruit borer'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Padagoda(BW-11)',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'Padagoda(BW-11)',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
 
         varieties_pest::create([
             'variety_name'=>'Padagoda(BW-11)',
             'pest_name'=>'Mite damage'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'SM-164',
             'pest_name'=>'Borer and fruit borer'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'SM-164',
             'pest_name'=>'Leaf-shrinking caterpillar'
 
-        ])
+        ]);
 
         varieties_pest::create([
             'variety_name'=>'SM-164',
             'pest_name'=>'Leaf aphid'
 
-        ])
+        ]);
 
         varieties_pest::create([
-            'variety_name'=>'SM-164',
-            'pest_name'=>'Mite damage'
+            'variety_name' => 'SM-164',
+            'pest_name' => 'Mite damage'
 
-        ])
+        ]);
     }
 }
