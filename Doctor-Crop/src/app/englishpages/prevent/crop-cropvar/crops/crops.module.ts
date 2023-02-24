f33e191a533202a3a12e6bb86da1899d73a0777e
iresha1234 <116697535+iresha1234@users.noreply.github.com>
2023-02-24 10:44:00 +0530

    Translation of 2 crop pages.

--- Doctor-Crop/src/app/englishpages/prevent/crop-cropvar/crops/crops.module.ts
+++ Doctor-Crop/src/app/englishpages/prevent/crop-cropvar/crops/crops.module.ts
@@ -8,13 +8,30 @@ import { CropsPageRoutingModule } from './crops-routing.module';
 
 import { CropsPage } from './crops.page';
 
+import {  HttpClient } from '@angular/common/http'; //1
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core'; 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'; //1
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+} //2
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CropsPageRoutingModule
-  ],
+    CropsPageRoutingModule,
+
+    TranslateModule.forChild({ //3
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
+  ], //3
+
   declarations: [CropsPage]
 })
 export class CropsPageModule {}
